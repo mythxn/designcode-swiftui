6677003ffaed02303ac276c81b8301cd85f588f3
Mithun <mythxn@icloud.com>
2019-07-11 15:04:59 +0400

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Mithun x on 7/11/19.
+//  Copyright © 2019 Mithun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
